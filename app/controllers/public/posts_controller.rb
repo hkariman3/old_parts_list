55242d1ad38ddbfd77bae86a48580003bc31f429
hkariman3 <hrmtk0@gmail.com>
2023-11-05 16:41:37 +0000

    postのshowまで

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -14,7 +14,16 @@ class Public::PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
 
+  def edit
+
+  end
+
+  def update
+  end
   private
 
   def post_params
