a669312df69669e2c71a30aec0bbd51e610e493b
hkariman3 <hrmtk0@gmail.com>
2024-05-07 04:34:43 +0000

    enum

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -2,4 +2,7 @@ class Contract < ApplicationRecord
   belongs_to :customer
   belongs_to :post
   belongs_to :address
+  
+  enum payment_status: {before_payment: 0, after_payment: 1, delivered: 2}
+  enum delivery_status: {waiting_payment: 0, shipped: 1, }
 end
