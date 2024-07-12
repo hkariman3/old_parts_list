7783327371ed7e7928ad4812500b83bea24d8ebf
hkariman3 <hrmtk0@gmail.com>
2024-07-12 14:26:34 +0000

    validate/confirm

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -6,4 +6,6 @@ class Contract < ApplicationRecord
   enum payment_status: {before_payment: 0, after_payment: 1, delivered: 2}, _default: "before_payment"
   enum delivery_status: {waiting_payment: 0, shipped: 1}, _default: "waiting_payment"
   enum contract_status: { uncompleted: 0, completed: 1}, _default: "uncompleted"
+  
+  validates :postage, presence: true
 end
