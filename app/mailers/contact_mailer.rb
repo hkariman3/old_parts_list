ca8d1b448718e085500d1701ec0ff1bc8cb3c37b
hkariman3 <hrmtk0@gmail.com>
2024-08-07 04:47:45 +0000

    action mailer/view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,3 +1,7 @@
 class ContactMailer < ApplicationMailer
-    
+    def send_when_admin_reply(customer, contact) #メソッドに対して引数を設定
+        @customer = user #ユーザー情報
+        @answer = contact.reply_text #返信内容
+        mail to: user.email, subject: '【OLP】 お問い合わせありがとうございます'
+    end
 end
