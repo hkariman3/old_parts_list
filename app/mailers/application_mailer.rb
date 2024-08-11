640f1228092cf48644e85372007ec52292db2efc
hkariman3 <hrmtk0@gmail.com>
2024-08-11 14:15:25 +0000

    action mailer/.env

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from:    "管理人 <hrmtk0@gmail.com>"
+  default from: 'from@example.com'
   layout 'mailer'
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,6 @@
 class ContactMailer < ApplicationMailer
-    def send_when_admin_reply(customer, contact) #メソッドに対して引数を設定
-        @customer = user #ユーザー情報
-        @answer = contact.reply_text #返信内容
-        mail to: user.email, subject: '【OLP】 お問い合わせありがとうございます'
+    def send_mail(contact)
+        @contact = contact
+        mail to:   ENV['TOMAIL'], subject: '【お問い合わせ】' + @contact.subject_i18n
     end
 end
