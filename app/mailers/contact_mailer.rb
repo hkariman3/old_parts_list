4e563009a30348adc5a739fa2164dbfc9852649a
hkariman3 <hrmtk0@gmail.com>
2024-09-27 14:49:44 +0000

    action mailer edit

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,6 @@
 class ContactMailer < ApplicationMailer
-    def send_when_admin_reply(customer, contact) #メソッドに対して引数を設定
-        @user = customer #ユーザー情報
-        @answer = contact.reply_text #返信内容
-        mail to: customer.email, subject: '【OPL】 お問い合わせありがとうございます'
+    def contact_mail(contact)
+    @contact = contact  
+    mail to: ENV['MAIL'], subject: "メールのタイトル"
     end
 end
